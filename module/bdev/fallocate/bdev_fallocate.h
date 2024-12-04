123cfc4cc533c019390b4d0c25fad2f560a63a54
Omar Pakker <unknown@unknown.com>
2024-12-04 02:09:42 +0100

    Add a dummy module just used to create files.
    Change-Id: Id10641358bb4c50f03e044010099fdc23b825c10

---
+++ module/bdev/fallocate/bdev_fallocate.h
@@ -0,0 +1,36 @@
+/*   SPDX-License-Identifier: BSD-3-Clause
+ *   Copyright (C) 2016 Intel Corporation.
+ *   All rights reserved.
+ *   Copyright (c) 2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ */
+
+#ifndef SPDK_BDEV_FALLOCATE_H
+#define SPDK_BDEV_FALLOCATE_H
+
+#include "spdk/stdinc.h"
+
+#include "spdk/bdev_module.h"
+
+typedef void (*spdk_bdev_fallocate_delete_complete)(void *cb_arg, int bdeverrno);
+
+struct bdev_fallocate_create_opts {
+	char *name;
+	struct spdk_uuid uuid;
+	char *filename;
+	uint64_t size;
+};
+
+struct bdev_fallocate_delete_opts {
+	char *name;
+};
+
+struct bdev_fallocate_resize_opts {
+	char *name;
+	uint64_t size;
+};
+
+int bdev_fallocate_create(const struct bdev_fallocate_create_opts *opts, struct spdk_bdev **bdev);
+void bdev_fallocate_delete(const struct bdev_fallocate_delete_opts *opts, spdk_bdev_fallocate_delete_complete cb_fn, void *cb_arg);
+int bdev_fallocate_resize(const struct bdev_fallocate_resize_opts *opts);
+
+#endif /* SPDK_BDEV_FALLOCATE_H */
