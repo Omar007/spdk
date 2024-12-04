6a7cdaa9dd3edf471b9af06ec918afef5abcbed8
Omar Pakker <unknown@unknown.com>
2024-12-04 02:09:42 +0100

    Add a dummy module just used to create files.
    Change-Id: Id10641358bb4c50f03e044010099fdc23b825c10

--- mk/spdk.lib_deps.mk
+++ mk/spdk.lib_deps.mk
@@ -150,6 +150,7 @@ DEPDIRS-bdev_compress := $(BDEV_DEPS_THREAD) reduce accel
 DEPDIRS-bdev_crypto := $(BDEV_DEPS_THREAD) accel
 DEPDIRS-bdev_delay := $(BDEV_DEPS_THREAD)
 DEPDIRS-bdev_error := $(BDEV_DEPS_THREAD)
+DEPDIRS-bdev_fallocate := $(BDEV_DEPS_THREAD)
 DEPDIRS-bdev_iscsi := $(BDEV_DEPS_THREAD)
 DEPDIRS-bdev_malloc := $(BDEV_DEPS_THREAD) accel dma
 DEPDIRS-bdev_null := $(BDEV_DEPS_THREAD)
